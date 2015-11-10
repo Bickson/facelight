e8d83fc174370dc79d706a65ab269c97e60f4a18
darioflorez <florez.wilmar@gmail.com>
2015-11-10 12:26:17 +0100

    create user method

--- src/main/java/handlers/UserHandler.java
+++ src/main/java/handlers/UserHandler.java
@@ -11,6 +11,12 @@ public class UserHandler {
     private static UserDAO userDAO = new UserDAO();
 
     public static User authenticateUser(String email, String password){
+        //return user info
         return userDAO.authenticateUser(email,password);
+    }
+
+    public static User createUser(User user){
+
+        return userDAO.create(user);
     }
 }
