56cd25567212185f6fac0e58115f38ac24c553d5
Johan Lundgren <johan.emil.lundgren@gmail.com>
2015-11-13 16:06:22 +0100

    fixed partials and added ajax to search

--- src/main/java/forms/MessageForm.java
+++ src/main/java/forms/MessageForm.java
@@ -1,7 +1,23 @@
 package forms;
 
+import models.MessageType;
+
 /**
  * Created by dario on 2015-11-12.
  */
 public class MessageForm {
+
+    private MessageType type;
+
+    public MessageType getType() {
+        return type;
+    }
+
+    public void setType(MessageType type) {
+        this.type = type;
+    }
+
+
+
+
 }
